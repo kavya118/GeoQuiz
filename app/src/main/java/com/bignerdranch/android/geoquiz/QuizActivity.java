282bb0b237badec5cc95d7a57b8f1fa9b1e1009a
Kavya Singh <kavyasecret@gmail.com>
2018-07-11 12:01:12 +0530

    After chapter 5 - before exercise. This works!

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,11 +17,14 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mpreviousButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
+    private boolean mIsCheater;
     private Question[] mQuestionBank = new Question[]
             {
                     new Question(R.string.question_australia,
@@ -32,6 +37,7 @@ public class QuizActivity extends AppCompatActivity {
                             true),
                     new Question(R.string.question_asia, true),
             };
+
     @Override
     public void onSaveInstanceState(Bundle
                                             savedInstanceState) {
@@ -39,7 +45,24 @@ public class QuizActivity extends AppCompatActivity {
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
+
     private int mCurrentIndex = 0;
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater =
+                    CheatActivity.wasAnswerShown(data);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,26 +76,26 @@ public class QuizActivity extends AppCompatActivity {
                 findViewById(R.id.question_text_view);          //Tying the text view with the code TextView
 
 
-        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new
                                                View.OnClickListener() {
                                                    @Override
                                                    public void onClick(View v) {
 
-                                                      checkAnswer(true);
+                                                       checkAnswer(true);
                                                    }
                                                });
 
-        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new
-                                               View.OnClickListener() {
-                                                   @Override
-                                                   public void onClick(View v) {
+                                                View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View v) {
 
-                                                       checkAnswer(false);
+                                                        checkAnswer(false);
 
-                                                   }
-                                               });
+                                                    }
+                                                });
 
         mNextButton = (Button)
                 findViewById(R.id.next_button);
@@ -82,6 +105,7 @@ public class QuizActivity extends AppCompatActivity {
                                                    public void onClick(View v) {
                                                        mCurrentIndex = (mCurrentIndex + 1) %
                                                                mQuestionBank.length;
+                                                       mIsCheater = false;
                                                        updateQuestion();
                                                    }
                                                });
@@ -92,13 +116,51 @@ public class QuizActivity extends AppCompatActivity {
                                                    View.OnClickListener() {
                                                        @Override
                                                        public void onClick(View v) {
-                                                           mCurrentIndex = (mQuestionBank.length+mCurrentIndex - 1)%
+                                                           mCurrentIndex = (mQuestionBank.length + mCurrentIndex - 1) %
                                                                    mQuestionBank.length;
                                                            updateQuestion();
                                                        }
                                                    });
         updateQuestion();
+        mCheatButton =
+                (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new
+                                                View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View v) {
+                                                        boolean answerIsTrue =
+                                                                mQuestionBank[mCurrentIndex].ismAnswerTrue();
+                                                        Intent intent =
+                                                                CheatActivity.newIntent(QuizActivity.this,
+                                                                        answerIsTrue);
+                                                        startActivityForResult(intent,
+                                                                REQUEST_CODE_CHEAT);
+                                                        /*How does the ActivityManager
+                                                        know which Activity to start? That
+                                                        information is in the Intent parameter.
+
+                                                        An intent is an object that a component
+                                                        can use to communicate with the OS.
+                                                        Extras are arbitrary data that the calling
+                                                         activity can include with an intent.
+
+                                                        The OS forwards the intent to
+                                                        the recipient activity, which can then
+                                                        access the extras and retrieve the data
+
+                                                        The OS forwards the intent to
+                                                        the recipient activity, which can then
+                                                        access the extras and retrieve the data,
+
+An extra is structured as a key-value
+pair, like the one you used to save out
+the value of mCurrentIndex in
+QuizActivity.onSaveInstanceState
+*/
+                                                    }
+                                                });
     }
+
 
     private void updateQuestion() {
         int question =
@@ -106,18 +168,21 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView.setText(question);
     }
 
-    private void checkAnswer(boolean userPressedTrue)
-    {
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue =
                 mQuestionBank[mCurrentIndex].ismAnswerTrue();
         int messageResId = 0;
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
-        }
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
 
+        }
         Toast.makeText(this, messageResId,
-                Toast.LENGTH_SHORT).show();
+                    Toast.LENGTH_SHORT).show();
+        }
     }
-}
